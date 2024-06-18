139653214ca93bfccb26d3a82ce82d037799cb09
Thafniss <t.cramossilva@gmail.com>
2024-06-18 17:29:49 -0300

    Update professor.php

--- ReservadeSalas/View/professor.php
+++ ReservadeSalas/View/professor.php
@@ -4,7 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Prodessor</title>
+    <title>Professor</title>
+    <style>
+    body {
+            background-color:#354f52; 
+        }
+        .btn-primary{
+        background-color: #cad2c5;
+        border-color: #cad2c5;
+        color: #354f52; 
+       }
+
+       .btn-primary:hover {
+       background-color:#354f52;
+       border-color: #cad2c5; 
+       }
+        .container {
+            min-height: 100vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            background-color:#84a98c;
+        }
+        h1 {
+            margin-bottom: 0.5em;
+            color:#354f52;
+        }
+        .form-label, .btn-primary, .btn-danger {
+            color:#354f52;
+        }
+        .table {
+            text-align: center;
+            color:#354f52; 
+        }
+    </style>
 </head>
 <body>
     <div class="container">
@@ -14,16 +48,15 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-2"></div>
-            <div class="col-8">
+            <div class="col-12">
                 <form action="controller/ProfessorController.php" method="post">
-                    <div class="mb-3">
+                    <div class="col-12 mb-3">
                         <label for="usuario" class="form-label">Usuario</label>
-                        <input type="text" name="ususario" class="form-control" required>
+                        <input type="text" name="usuario" class="form-control " required>
                     </div>
-                    <div class="mb-3">
+                    <div class="col-12 mb-3">
                         <label for="senha" class="form-label">Senha</label>
-                        <input type="text" name="senha" class="form-control">
+                        <input type="text" name="senha" class="form-control ">
                     </div>
                     <input type="submit" name="cadastrar" value="Salvar" class="btn btn-primary">
                 </form>
@@ -42,8 +75,12 @@
                       </tr>
                     </thead>
                     <tbody>
+                    <?php 
+                    include_once "C:\\xampp\\htdocs\\ReservaSalas\\ReservadeSalas\\dao\\ProfessorDao.php";
 
-                      <?php foreach ($CadastroDao->read() as $Professor) :?>
+
+                    $ProfessorDao = new ProfessorDao();
+                    foreach ($ProfessorDao->read() as $Professor) :?>
                         <tr>
                         <td><?= $Professor->getId() ?></td>
                         <td><?= $Professor->getUsuario() ?></td>
